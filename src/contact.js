5072271cd92baccd3316f20997c2551b6cf2b28c
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-06 16:13:55 +0800

    Generate contact information

--- src/contact.js
+++ src/contact.js
@@ -1,17 +1,28 @@
 export const Contact = (function() {
     const load = () => {
-        const headline = document.createElement("h1");
-        headline.textContent = "Contact Us";
-        content.appendChild(headline);
+    const contactInformation = [
+            {name: "Contact Us", details: ["Telephone: +60 2390 7584", "Email: catamari_restaurant@catamari.meow"]},
+            {name: "Address", details: ["390 Purring Meow", "Kuching, Sarawak", "Malaysia"]},
+            {name: "Hours", details: ["Monday: 10:00AM - 8:00PM", "Tuesday: 10:00AM - 8:00PM", "Wednesday: Closed for Meownovations", 
+                                    "Thursday: 10:00AM - 8:00PM", "Friday: 10:00AM - 10:00PM", "Saturday: 9:00AM - 10:00PM", "Sunday: 9:00AM - 10:00PM"]}
+        ]
 
+        contactInformation.forEach((contact) => {
+            const container = document.createElement("div");
+            content.appendChild(container);
 
-        const hoursHeader = document.createElement("p");
-        hoursHeader.textContent = "Mon-Fri 10am-10pm";
-        content.appendChild(hoursHeader);
+            const header = document.createElement("h2");
+            header.textContent = `${contact.name}`;
+            container.appendChild(header);
 
-        const hours = document.createElement("p");
-        hours.textContent = "Mon-Fri 10am-10pm";
-        content.appendChild(hours);
+            contact.details.forEach((line) => {
+                const info = document.createElement("p");
+                info.textContent = `${line}`;
+                container.appendChild(info);
+            })
+        });
+
+ 
     };
 
     return { load };
