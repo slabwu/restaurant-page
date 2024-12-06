bc872769ac444f75f63742b406da0adb427eb3b3
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-06 13:35:21 +0800

    Prevent reloading of current page

--- src/index.js
+++ src/index.js
@@ -5,24 +5,34 @@ import { About } from "./about.js";
 
 const Display = (function() {
     const content = document.getElementById("content");
+    let context = "home";
     Home.load();
 
     const homeButton = document.getElementById("home");
     homeButton.addEventListener("click", e => {
-        clearPage();
-        Home.load();
+        if (context !== "home") {
+            clearPage();
+            Home.load();
+            context = 'home';
+        };
     });
 
     const menuButton = document.getElementById("menu");
     menuButton.addEventListener("click", e => {
-        clearPage();
-        Menu.load();
+        if (context !== "menu") {
+            clearPage();
+            Menu.load();
+            context = 'menu';
+        };
     });
 
     const aboutButton = document.getElementById("about");
     aboutButton.addEventListener("click", e => {
-        clearPage();
-        About.load();
+        if (context !== "about") {
+            clearPage();
+            About.load();
+            context = 'about';
+        };
     });
 
 
