70d0d105a37d3c6d046415661c01747807c543a3
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-06 12:39:04 +0800

    Refactor IIFE

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./styles.css";
 
-const loadPage = (function() {
-    const content = document.getElementById("content");
+const content = document.getElementById("content");
+const home = document.getElementById("home");
+const menu = document.getElementById("menu");
+const about = document.getElementById("about");
 
+const loadPage = () => {
     const headline = document.createElement("h1");
     headline.textContent = "Welcome to Restaurant";
     content.appendChild(headline);
-})();
+};
+
+loadPage();
