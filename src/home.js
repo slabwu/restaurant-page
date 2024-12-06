faa04bd2f8e5c7d527eb546d0fe65b73c8a97bf6
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-06 15:58:09 +0800

    Add homepage description

--- src/home.js
+++ src/home.js
@@ -4,6 +4,10 @@ export const Home = (function() {
         headline.textContent = "Only the best seafood a for the best felines.";
         content.appendChild(headline);
 
+        const paragraph = document.createElement("p");
+        paragraph.textContent = "Discover a world of flavour right at your toe-beans. You'll be sure to come back purr more.";
+        content.appendChild(paragraph);
+
         const button = document.createElement("button");
         button.textContent = "Order Today";
         content.appendChild(button);
