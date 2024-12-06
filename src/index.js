74c343342f2072a4a4074aea541c4066c52933d0
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-06 12:26:03 +0800

    Create loadPage IIFE

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+
+const loadPage = (function() {
+    const content = document.getElementById("content");
+
+    const headline = document.createElement("h1");
+    headline.textContent = "Welcome to Restaurant";
+    content.appendChild(headline);
+})();
