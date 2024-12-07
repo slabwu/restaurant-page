319210cb2f21807eaa3740cf67d4e8a1c7602a65
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-07 11:21:30 +0800

    Add homepage image

--- src/home.js
+++ src/home.js
@@ -1,18 +1,23 @@
 export const Home = (function() {
     const load = () => {
         const headline = document.createElement("h2");
-        headline.textContent = "Only the best seafood for the best felines.";
+        headline.textContent = "Only the best seafood for the best of felines.";
         content.appendChild(headline);
 
         const paragraph = document.createElement("p");
-        paragraph.textContent = "Discover a world of flavour right at your toe-beans. You'll be sure to come back purr more.";
+        paragraph.textContent = "Discover a world of flavour beneath at your toe-beans. You'll be sure to come back purr more.";
         content.appendChild(paragraph);
 
         const button = document.createElement("button");
         button.textContent = "Order Today";
         content.appendChild(button);
     
+        const image = document.createElement("img");
+        image.src = cat;
+        content.appendChild(image);
     };
 
     return { load };
 })();
+
+import cat from "./assets/cat.jpg";
