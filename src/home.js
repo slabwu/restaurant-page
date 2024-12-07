7413b24b0784ae47b2a7c98d0c046a47d83bbdf6
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-07 11:24:20 +0800

    Create .home class

--- src/home.js
+++ src/home.js
@@ -1,18 +1,22 @@
 export const Home = (function() {
     const load = () => {
         const headline = document.createElement("h2");
+        headline.classList.add("home");
         headline.textContent = "Only the best seafood for the best of felines.";
         content.appendChild(headline);
 
         const paragraph = document.createElement("p");
+        paragraph.classList.add("home");
         paragraph.textContent = "Discover a world of flavour beneath at your toe-beans. You'll be sure to come back purr more.";
         content.appendChild(paragraph);
 
         const button = document.createElement("button");
+        button.classList.add("home");
         button.textContent = "Order Today";
         content.appendChild(button);
     
         const image = document.createElement("img");
+        image.classList.add("home");
         image.src = cat;
         content.appendChild(image);
     };
